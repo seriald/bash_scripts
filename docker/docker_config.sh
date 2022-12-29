745b71b54cfb27a401e4705e5f7fae4d4302b796
Robert Fleming <robert.fleming@vitori.org>
2022-12-29 15:11:07 -0500

    Docker scripts

---
+++ docker/docker_config.sh
@@ -0,0 +1,5 @@
+echo "Deploying Portainer Agent"
+sudo docker run -d   -p 9001:9001   --name portainer_agent_docks.vitori.org   --restart=always   -v /var/run/docker.sock:/var/run/docker.sock   portainer/agent:latest
+echo ""
+echo "Deploying Watch Tower"
+sudo docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
