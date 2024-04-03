f7559d106885663890bc49a9e38bd525bd3dec05
Robert Flemnig <robert.fleming@vitori.org>
2024-04-02 22:18:24 -0400

    RSAT Tools

--- Windows/win11_installRSAT.ps1
+++ Windows/win11_installRSAT.ps1
@@ -1,14 +1,14 @@
 <#
-=============================================================================================
-Name:           
-Description:    
-Version:        0.1
-website:        
-Script by:      Robert Fleming
-Date:           2023-09-09
-For detailed Script execution: 
-============================================================================================
-#>
+        =============================================================================================
+        Name:           
+        Description:    
+        Version:        0.1
+        website:        
+        Script by:      Robert Fleming
+        Date:           2023-09-09
+        For detailed Script execution: 
+        ============================================================================================
+        #>
 
 $admincheck = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
@@ -18,19 +18,23 @@ If ($admincheck -like "false") {
 }
 
 Else {
-    
-    $rsatavail = Get-WindowsCompatibility -name RSAT* -online
+            
+    $rsatavail = Get-WindowsCapability -Name RSAT* -Online | Select-Object -Property DisplayName, State
 
-    ForEach ($rsat in $rsatavail){
+    $rsatavail
+    ForEach ($rsat in $rsatavail) {
+        $rsat
+        
         $status = $rsat.state
         $display = $rsat.displayname
-        if ($state -eq "Installed"){
-            Write-Host "$display already installed"
+
+        if ($state -like "NotPresent") {
+            Get-WindowsCompatibility -name $display -online | Add-WindowsCapability -Online
+            Write-Host "$display installed"
         }
 
-        Else {
-            $rsatavail = Get-WindowsCompatibility -name $rsat -online
-            Write-host "Installed $display "
+        If ($status -like "Installed") {
+            Write-Host "$display previously installed"
         }
     }
 }
