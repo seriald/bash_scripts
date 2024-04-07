99b371f9dbecaf42d14baf9da8cc2d2c8533a4b6
Robert Flemnig <robert.fleming@vitori.org>
2024-04-06 20:25:27 -0400

    DHCP Backup

--- Windows/DHCP/DHCP_ConfigBackup.ps1
+++ Windows/DHCP/DHCP_ConfigBackup.ps1
@@ -1,4 +1,41 @@
-$DHCPServer = Read-Host "Please enter DHCP Server:"
+<#
+=============================================================================================
+Name:           
+Description:    
+Version:        0.1
+website:        
+Script by:      Robert Fleming
+Date:           
+For detailed Script execution: 
+============================================================================================
+#>
 
-Export-DhcpServer -ComputerName "$DHCPServer" -File "C:\temp\dhcpexport.xml"
-# Backup-DhcpServer -ComputerName "$DHCPServer"
+function Get_Date {
+    
+    return "{0:yyyyMMdd}" -f (Get-Date)
+    
+}
+
+$DHCPServer = Read-Host "Please enter DHCP Server"
+$current = Get-Location
+$Outfile = "$current\$(Get_Date)_DHCPExport.xml"
+
+if (Test-Path $OutFile -PathType Leaf) {
+    Write-Host "Clearing previous output file $Outfile" -ForegroundColor Red
+    Write-Host ""
+    del $outfile
+}
+
+$confirm = Test-Connection $DHCPServer
+
+if ($null -like $confirm) {
+    Write-Host "$dhcpserver not reachable"
+    
+}
+
+Else {
+    Export-DhcpServer -ComputerName "$DHCPServer" -File $outfile
+    #Backup-DhcpServer -ComputerName "$DHCPServer" -File $outfile
+
+    Write-Host "Exported $dhcpserver configuration to $outfile"
+}
