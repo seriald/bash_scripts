f333f50c12b22b7eab2b5ae3791f69780bc3c887
Robert Fleming <robert@Roberts-MacBook-Pro.local>
2024-07-13 23:13:05 -0300

    20240713

--- Tools/RM_RemoteRDP_ssh.sh
+++ Tools/RM_RemoteRDP_ssh.sh
@@ -1,12 +1,5 @@
-echo "Looking for any SSH Tunnel's"
-ps | grep "ssh -L" | grep -v grep
+echo "Looking for SSH Tunnels"
+ps |grep "ssh -L"|grep -v grep
 echo ""
-echo "Do you see the connection you want to kill"
-read confirm
-
-if [[ ($confirm -eq Y) || ($confirm -eq y) ]]
-echo ""
-echo 'Do you want to kill all the tunnels'
-read confirm2
-
-fi
+echo "Killing all SSH Tunnels' currently in use"
+ps |grep "ssh -L"|grep -v grep|awk '{print $1}'|xargs kill
