4633c353a0eb49923eed62e6b78b0cefc5705acc
Robert Fleming <robert.fleming@vitori.org>
2024-07-21 22:36:59 -0300

    20240721

--- Tools/RM_RemoteRDP_ssh.sh
+++ Tools/RM_RemoteRDP_ssh.sh
@@ -1,7 +1,5 @@
 echo "Looking for SSH Tunnels"
 ps |grep "ssh -L"|grep -v grep
-test = ps |grep "ssh -L"|grep -v grep
-
 echo ""
 echo "Killing all SSH Tunnels' currently in use"
 ps |grep "ssh -L"|grep -v grep|awk '{print $1}'|xargs kill
