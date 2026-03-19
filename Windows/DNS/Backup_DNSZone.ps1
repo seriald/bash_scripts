d432f4a1cdd3f16688c29ee417a739f5f7193ce3
Robert Flemnig <robert.fleming@vitori.org>
2026-03-19 10:22:07 -0400

    20260319 DNS Backups

---
+++ Windows/DNS/Backup_DNSZone.ps1
@@ -0,0 +1,54 @@
+<#
+=============================================================================================
+Name:           Backup_DNSZone.ps1
+Description:    Exports DNS Zone to CSV
+Version:        1.0
+website:        
+Script by:      Robert Fleming
+Date:           2026-03-19
+For detailed Script execution: 
+============================================================================================
+#>
+
+#Functions
+function Get_Date {
+    
+  return "{0:yyyyMMdd}" -f (Get-Date)
+    
+}
+
+#Variables
+
+$Server = "picklerick.vitori.org"
+
+
+#Core
+
+$admincheck = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+If ($admincheck -like "false") {
+
+  Write-Host "Run from Administrator Console"
+}
+
+Else {
+
+$Zones = Get-DnsServerZone | Where-Object { ($_.ZoneName -notlike "*arpa*") -and ($_.ZoneName -notlike "*msdcs*") -and ($_.ZoneName -notlike "*TrustAnchors*") } | Select-Object -ExpandProperty ZoneName
+
+  ForEach ($Zone in $Zones) {
+    $Outfolder = "c:\windows\system32\dns" 
+    $Outfile = "$(Get_Date)_$Zone"
+
+    if (Test-Path "$OutFolder\$Outfile" -PathType Leaf) {
+      Write-Host "Clearing previous output file $Outfile" -ForegroundColor Red
+      Write-Host ""
+      Remove-Item $OutFolder\$Outfile
+    }
+
+    Export-DnsServerZone -Name $Zone -FileName "$Outfile"
+
+  }
+
+  Write-Host "Exporting results to $OutFolder\$Outfile" -ForegroundColor Cyan
+
+}
